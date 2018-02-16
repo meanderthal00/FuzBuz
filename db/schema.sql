ec99acaf7f68668d79438dc73751a88f9910ce8c
William Amato <IamWill216@yahoo.com>
2018-02-15 21:12:22 -0500

    practice push plus initial sql schema

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- creation of our database 
+CREATE DATABASE fuzbuz;
+USE fuzbuz;
+
+-- this will hold our user information
+CREATE TABLE fuzers
+    id INT AUTO_INCREMNT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(55) NOT NULL
+    ();
+
+-- im going to test pushing now...if i mess up please don't hate me ryan
