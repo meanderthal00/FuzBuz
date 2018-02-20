ad73e4477a200235a8f7351f023820d1449c95ea
Meanderthal00 <meanderthal00@yahoo.com>
2018-02-20 07:01:25 -0500

    trying to add a clear button for canvas, and working on side bar

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,25 +1,25 @@
-console.log("front end js connected");
+// console.log("front end js connected");
 // scripts for the face-doodle
 
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
- console.log("i am 1");
+//  console.log("i am 1");
 var painting = document.getElementById('paint');
-console.log("i am 2");
+// console.log("i am 2");
 var paint_style = getComputedStyle(painting);
 canvas.width = parseInt(paint_style.getPropertyValue('width'));
 canvas.height = parseInt(paint_style.getPropertyValue('height'));
-console.log("I am 3");
+// console.log("I am 3");
 var mouse = {x: 0, y:-50};
  
 canvas.addEventListener('mousemove', function(e) {
-    console.log("mouse move ");
+    // console.log("mouse move ");
 //   mouse.x = e.pageX + $(this).offset().left - $(this).position().left;
 mouse.x = e.pageX  -  $(this).offset().left
-console.log("pageX " +e.pageX);
+// console.log("pageX " +e.pageX);
 //   mouse.y = e.pageY + $(this).offset().top - $(this).position().top;
 mouse.y = e.pageY - $(this).offset().top
-  console.log("pageY " + e.pageY);
+//   console.log("pageY " + e.pageY);
 }, false);
 
 ctx.lineWidth = 10;
@@ -43,10 +43,6 @@ var onPaint = function() {
     ctx.stroke();
 };
 
-// script for the shake
-// $( document ).hover(function() {
-//     $( "#toggle" ).effect( "shake" );
-//   });
 
   // script for modal 
   $("#login").click(function() {
@@ -63,3 +59,8 @@ var onPaint = function() {
    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //    $('.collapsible').collapsible();
    
+
+// clear canvas button
+document.getElementById('clear').addEventListener('click', function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }, false);
